9e1fb193c7c67bc499e2935a34ffe4e88cb5e66e
shunzi <tobyzsj@gmail.com>
2024-07-08 20:09:50 +0800

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import Unocss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/framer-motion-test",
   plugins: [react(), Unocss()],
   build: {
     outDir: "docs",
